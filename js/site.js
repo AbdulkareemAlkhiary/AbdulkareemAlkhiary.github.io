4aad344b8deac7f4511e3403d9bd32b1babd1a1a
Abdulkareem Alkhiary <araasai@ksu.edu>
2021-04-23 17:42:14 -0500

    Uploaded Website

---
+++ js/site.js
@@ -0,0 +1,230 @@
+﻿
+/////////////////////////// Hiding Header /////////////////////////
+(function(){
+
+    var doc = document.documentElement;
+    var w   = window;
+
+    /*
+    define four variables: curScroll, prevScroll, curDirection, prevDirection
+    */
+
+    var curScroll;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    /*
+    how it works:
+    -------------
+    create a scroll event listener
+    create function to check scroll position on each scroll event,
+    compare curScroll and prevScroll values to find the scroll direction
+    scroll up - 1, scroll down - 2, initial - 0
+    then set the direction value to curDirection
+    compare curDirection and prevDirection
+    if it is different, call a function to show or hide the header
+    example:
+    step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
+    step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
+    step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
+    */
+
+    var header = document.getElementById('site-header');
+    var toggled;
+    var threshold = 200;
+
+    var checkScroll = function() {
+        curScroll = w.scrollY || doc.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
+
+
+
+
+
+
+/////////////////////////// Loading page before actual page loads /////////////////////////
+$(window).on("load", function () {
+    $(".loader-wrapper").fadeOut(2500);
+});
+
+
+///////////////////////// Fade In Feature /////////////////////////
+$(function () {  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+});
+$(document).ready(function () {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.hideme').each(function (i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it */
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({ 'opacity': '1' }, 500);
+
+            }
+
+        });
+
+    });
+
+});
+
+
+
+
+/////////////////////// Contact Form /////////////////////////////
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty();
+        if (name.length >= 2) {
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Name is not valid</div>')
+        }
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>')
+        }
+        if (message.length >= 10) {
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+    })
+})
+
+
+
+
+/////////////////////// Bouncing Ball /////////////////////////////
+// Some random colors
+const colors = ["#F0C808", "#07A0C3", "#EC4067", "#A01A7D", "#FFFFFF"];
+
+const numBalls = 175;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+    let ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    ball.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `${Math.random()}em`;
+    ball.style.height = ball.style.width;
+
+    balls.push(ball);
+    document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+    let to = {
+        x: Math.random() - 2,
+        y: Math.random() * 200
+    };
+
+    let anim = el.animate(
+        [
+            { transform: "translate(0, 0)" },
+            { transform: `translate(${to.x}rem, ${to.y}rem)` }
+        ],
+        {
+            duration: (Math.random() + 1) * 2000, // random duration
+            direction: "alternate",
+            fill: "both",
+            iterations: Infinity,
+            easing: "ease-in-out"
+        }
+    );
+});
+
+
+/////////////////////// Hero Section Typing /////////////////////////////
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Abdulkareem Adnan Alkhiary", "Computer Engineer", "Innovator"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
